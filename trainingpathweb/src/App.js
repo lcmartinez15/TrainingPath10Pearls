efd52b9e7cf395bb28b477795ce56195456fa046
Laura Martinez <laura.martinez@10pearls.com>
2020-05-13 01:16:13 -0500

    Add react redux to project

--- trainingpathweb/src/App.js
+++ trainingpathweb/src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+//redux
+import { Provider } from "react-redux";
+import store from "../src/redux/store";
+
+import Login from "../src/pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <Login />
+        </header>{" "}
+      </div>
+    </Provider>
   );
 }
 
